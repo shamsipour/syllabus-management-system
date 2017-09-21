86a990eb5191928bf6930a8e8f3ccdf8f6518683
Erfan Sahafnejad <erfan.sahaf@gmail.com>
2017-09-21 13:57:49 +0330

    Wrap 3 API endpoints into RouteGroup

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@
 
     Route::group(['prefix' => config('system.API_PATH'), 'namespace' => 'API'], function() {
         Route::get('plans', 'MainController@getPlans');
+
+        Route::get('times', function () {return \App\Time::all();});
+        Route::get('majors', function () {return \App\Major::all();});
+        Route::get('lessons', function () {return \App\Lesson::all();});
     });
-    Route::get('times', function () {return \App\Time::all();});
-    Route::get('majors', function () {return \App\Major::all();});
-    Route::get('lessons', function () {return \App\Lesson::all();});
