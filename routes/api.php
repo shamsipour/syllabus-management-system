c4b2a2ebaff0698da2bed059101f951e1b89c857
Erfan Sahafnejad <erfan.sahaf@gmail.com>
2017-09-22 14:17:26 +0330

    ref: Remove default comments in API routes file

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,5 @@
 <?php
 
-    use Illuminate\Http\Request;
-
-    /*
-    |--------------------------------------------------------------------------
-    | API Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register API routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | is assigned the "api" middleware group. Enjoy building your API!
-    |
-    */
-
     Route::group(['prefix' => config('system.API_PATH'), 'namespace' => 'API'], function() {
         Route::get('plans', 'MainController@getPlans');
 
