ca56d73178e369bf70f3a560e3955239c4ec44d8
Erfan Sahafnejad <erfan.sahaf@gmail.com>
2017-09-22 07:22:29 +0330

    feat: Filter Plans based on day or major in /plans endpoint

--- app/Http/Controllers/API/MainController.php
+++ app/Http/Controllers/API/MainController.php
@@ -1,41 +1,54 @@
 <?php
 
-namespace App\Http\Controllers\API;
+    namespace App\Http\Controllers\API;
 
-use App\Plan;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
+    use App\Major;
+    use App\Plan;
+    use Illuminate\Http\Request;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Support\Facades\DB;
 
-class MainController extends Controller
-{
-
-    // Handle plans API endpoint
-    public function getPlans(Request $request)
+    class MainController extends Controller
     {
-        // Get day from query string
-        $day = $request->get('day', null);
 
+        // Handle plans API endpoint
+        public function getPlans(Request $request)
+        {
+            // Get day from query string
+            $day = $request->get('day', null);
+            $major_id = $request->get('major_id', null);
 
-        // Return if day was not an integer or it was greater than 6/less than 0
-        if($day == null || !is_int((int)$day) || $day > 6 || $day < 0)
-            return ['success' => false, 'message' => 'You should pass a specific day as a query string parameter in Integer type', 'data' => null];
+            // Get all plans
+            $plans = Plan::join('times', 'times.id', '=', 'plans.time_id')
+                ->join('teachers', 'teachers.id', '=', 'plans.teacher_id')
+                ->join('lessons', 'lessons.id', '=', 'plans.lesson_id')
+                ->join('majors', 'majors.id', '=', 'lessons.major_id')
+                ->select('plans.class', 'plans.day', 'times.start', 'times.end', 'lessons.name as lesson_name', DB::raw('CONCAT(teachers.name, " ", teachers.family) AS teacher_name'), 'majors.name as major_name')
+                ->orderBy('times.start')
+                ->orderBy('times.end')
+                ->orderBy('lessons.name')
+                ->orderBy('majors.name');
 
-        // Get plans of a specific day based on Major ID
-        $plans = Plan::
-        join('times', 'times.id', '=', 'plans.time_id')
-            ->join('teachers', 'teachers.id', '=', 'plans.teacher_id')
-            ->join('lessons', 'lessons.id', '=', 'plans.lesson_id')
-            ->join('majors', 'majors.id', '=', 'lessons.major_id')
-            ->select('plans.class', 'times.start', 'times.end', 'lessons.name as lesson_name', DB::raw('CONCAT(teachers.name, " ", teachers.family) AS teacher_name'), 'majors.name as major_name')
-            ->where('day', $day)
-            ->orderBy('times.start')
-            ->orderBy('times.end')
-            ->orderBy('lessons.name')
-            ->orderBy('majors.name')
-            ->get();
+            // Filter plans based on their day
+            if ($day != null && is_int((int)$day) && $day <= 6 && $day >= 0)
+                $plans = $plans->where('plans.day', (int)$day);
+            // Filter plans based on their major
+            if ($major_id != null && is_int((int)$major_id))
+                $plans = $plans->where('majors.id', (int)$major_id);
 
-        $caption = 'برنامه روز '.config('system.DAYS')[$day];
-        return ['success' => true, 'caption' => $caption, 'data' => $plans];
+            $plans = $plans->get();
+
+            $caption = 'برنامه کلاس های ';
+            if($day != null || $major_id != null){
+                $caption .= $day != null ? 'روز ' . config('system.DAYS')[$day] . ' ' : '';
+                if($major_id != null){
+                    $major = Major::findOrFail($major_id)->first();
+                    $caption .= 'رشته '.$major->name;
+                    $caption .= ' مقطع '.$major->level_caption;
+                }
+            }
+            else
+                $caption .= 'هفتگی دانشگاه';
+            return ['success' => true, 'caption' => trim($caption), 'data' => $plans];
+        }
     }
-}
